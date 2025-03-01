7acc1785cc88e2af01e13887a97c08c9d33d4f7a
asmund20 <asmund20@gmail.com>
2025-03-01 17:37:52 +0100

    Format all files

--- inode.h
+++ inode.h
@@ -1,19 +1,18 @@
 #ifndef INODE_H
 #define INODE_H
 
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
 
 /*******************************************************************************
  * BEGIN: ADD YOUR OWN STRUCT AND MACROS BELOW HERE
  ******************************************************************************/
 
-struct Extent
-{
-    uint32_t blockno;
-    uint32_t extent;
+struct Extent {
+  uint32_t blockno;
+  uint32_t extent;
 };
 
 /*******************************************************************************
@@ -28,15 +27,14 @@ struct Extent
  * when is_directory==1 and that you must interpret
  * as block numbers when is_directory==0.
  */
-struct inode
-{
-	uint32_t   id;
-	char*      name;
-	char       is_directory;
-	char       is_readonly;
-	uint32_t   filesize;
-	uint32_t   num_entries;
-	uintptr_t* entries;
+struct inode {
+  uint32_t id;
+  char *name;
+  char is_directory;
+  char is_readonly;
+  uint32_t filesize;
+  uint32_t num_entries;
+  uintptr_t *entries;
 };
 
 /* Create a file below the inode parent. Parent must
@@ -46,23 +44,21 @@ struct inode
  * disk to store all of these bytes.
  * Returns a pointer to file's inodes.
  */
-struct inode* create_file( struct inode* parent,
-                           const char* name,
-                           char readonly,
-                           int size_in_bytes );
+struct inode *create_file(struct inode *parent, const char *name, char readonly,
+                          int size_in_bytes);
 
 /* Create a directory below the inode parent. Parent must
  * be a directory.
  * Returns a pointer to file's inodes.
  */
-struct inode* create_dir( struct inode* parent, const char* name );
+struct inode *create_dir(struct inode *parent, const char *name);
 
 /* Check all the inodes that are directly referenced by
  * the node parent. If one of them has the name "name",
  * its inode pointer is returned.
  * parent must be directory.
  */
-struct inode* find_inode_by_name( struct inode* parent, const char* name );
+struct inode *find_inode_by_name(struct inode *parent, const char *name);
 
 /* Delete the file given by its inode, if it is an inode
  * directly referenced by parent.
@@ -70,27 +66,27 @@ struct inode* find_inode_by_name( struct inode* parent, const char* name );
  * referenced by this file. This removes those blocks from
  * simulate disk.
  */
-int delete_file( struct inode* parent, struct inode* node );
+int delete_file(struct inode *parent, struct inode *node);
 
 /* Delete the directory given by its inode, if it is an inode
  * directly referenced by parent.
  * The function fails if the node is still referencing other
  * inodes.
  */
-int delete_dir( struct inode* parent, struct inode* node );
+int delete_dir(struct inode *parent, struct inode *node);
 
 /* Write the given inode root and all inodes referenced by it
  * to the master file table, following the oblig instructions.
  * No inodes are changed.
  */
-void save_inodes( const char* master_file_table, struct inode* root );
+void save_inodes(const char *master_file_table, struct inode *root);
 
 /* Read the file master file table and create an inode in memory
  * for every inode that is stored in the file. Set the pointers
  * between inodes correctly.
  * The file containing the master file table remains unchanged.
  */
-struct inode* load_inodes( const char* master_file_table );
+struct inode *load_inodes(const char *master_file_table);
 
 /* This function releases all dynamically allocated memory
  * recursively for all the referenced inodes are visited
@@ -102,14 +98,14 @@ struct inode* load_inodes( const char* master_file_table );
  * This function can be used to end a program after
  * save_inodes and helps you to avoid valgrind errors.
  */
-void fs_shutdown( struct inode* node );
+void fs_shutdown(struct inode *node);
 
 /* This function is handed out.
  *
  * It prints the node that is receives as a parameter,
  * and recurivesly all inodes that are stored below it.
  */
-void debug_fs( struct inode* node );
+void debug_fs(struct inode *node);
 
 /*******************************************************************************
  * BEGIN: ADD YOUR OWN FUNCTION DECLARATIONS BELOW HERE
@@ -120,4 +116,3 @@ void debug_fs( struct inode* node );
  ******************************************************************************/
 
 #endif
-
