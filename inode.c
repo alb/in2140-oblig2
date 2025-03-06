7b6158725ff23fb77c0f643db16574f927005cb9
asmund20 <asmund20@gmail.com>
2025-03-06 18:31:07 +0100

    Revert previous typo fix that was not a typo

--- inode.c
+++ inode.c
@@ -302,7 +302,7 @@ char *save_inodes_recursive(char *writer, struct inode *inode) {
   *writer = (*inode).is_readonly;
   writer++;
 
-  writer = write(writer.(*inode).num_entries);
+  writer = write(writer, (*inode).num_entries);
   for (int i = 0; i < (*inode).num_entries; i++) {
     if ((*inode).is_directory) {
 	writer = write(writer, (*inode).entries[i].id);
